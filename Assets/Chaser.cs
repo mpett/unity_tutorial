5afcbc6eaa48f45307aff793e3d44480f3d55557
Martin Pettersson <kontakt@martinpettersson.se>
2016-05-28 06:16:18 +0200

    Chaser

---
+++ Assets/Chaser.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chaser : MonoBehaviour {
+
+	public Transform targetTransfrom;
+	float speed = 7;
+
+	void Update() {
+		Vector3 displacementFromTarget = targetTransfrom.position - transform.position;
+		Vector3 directionToTarget = displacementFromTarget.normalized;
+		Vector3 velocity = directionToTarget * speed;
+
+		transform.Translate (velocity * Time.deltaTime);
+	}
+}
