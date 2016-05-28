feb77f9e8238a8a663bfe43b111b35d7a6de383b
Martin Pettersson <kontakt@martinpettersson.se>
2016-05-28 06:46:20 +0200

    Rotation script

---
+++ Assets/CubeScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeScript : MonoBehaviour {
+
+	public Transform sphereTransform;
+
+	// Use this for initialization
+	void Start () {
+		sphereTransform.parent = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.up * Time.deltaTime * 180);
+	}
+}
