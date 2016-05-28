0bd7c1835078466f5c184fbb9210b89ca8c147f1
Martin Pettersson <kontakt@martinpettersson.se>
2016-05-28 06:00:08 +0200

    Initial commit

---
+++ Assets/Player.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	float speed = 10;
+
+	void Update() {
+		Vector3 input = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical"));
+		Vector3 direction = input.normalized;
+		Vector3 velocity = direction * speed;
+		Vector3 moveAmount = velocity * Time.deltaTime;
+
+		transform.Translate (moveAmount);
+	}
+}
---
+++ Assets/test.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	int frameCount = 0;
+
+	// Use this for initialization
+	void Start () {
+		print ("Start");
+		float distance = getDistanceBetweenPoints (0, 5, 10, 15);
+		print (distance);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		frameCount += 1;
+		// print ("Frame count " + frameCount);
+	}
+
+	float getDistanceBetweenPoints(float x1, float y1, float x2, float y2) {
+		float dX = x2 - x1;
+		float dY = y2 - y1;
+		float distanceSquared = dX * dX + dY * dY;
+		float distance = Mathf.Sqrt (distanceSquared);
+		return distance;
+	}
+
+
+}
