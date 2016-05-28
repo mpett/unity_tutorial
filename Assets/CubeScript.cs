b81c46feff27ca6005e3a7f6adef5fdb4bf72e7d
Martin Pettersson <kontakt@martinpettersson.se>
2016-05-28 06:56:23 +0200

    Finished rotate script

--- Assets/CubeScript.cs
+++ Assets/CubeScript.cs
@@ -8,10 +8,16 @@ public class CubeScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		sphereTransform.parent = transform;
+		sphereTransform.localScale = Vector3.one * 2;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate (Vector3.up * Time.deltaTime * 180);
+		transform.Rotate (Vector3.up * Time.deltaTime * 180, Space.World );
+		transform.Translate (Vector3.forward * Time.deltaTime * 7, Space.World);
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			sphereTransform.localPosition = Vector3.zero;
+		}
 	}
 }
